87e033e5564e56e1892eee294895142f2b2baf98
Laion Gabriel <laiong.dev@gmail.com>
2023-01-16 03:41:09 -0300

    refactoring

--- functions.js
+++ functions.js
@@ -1,4 +1,15 @@
 import Countdown from "./countdown.js";
+import { PermissionsBitField } from "discord.js";
+
+export function getHelp(message) {
+   return `Olá, ${message.author.username}!\nComandos legais:\n\n\`!cookie\`: frase inspiradora de autores famosos.\n\`!clean\`: limpa mensagens recentes do chat atual.\n\`!cat\`: imagens aleatórias de gatinhos.\n\`!dog\`: imagens e gifs aleatórios de doguinhos.\n\`!bible\`: versículos da bíblia.\n\nO malvadão ainda está trabalhando em mais comandos.`;
+}
+
+export function ferias() {
+   const UfraEndOfSemester = new Countdown("19 May 2023 23:59:59 GMT-0300");
+
+   return `Faltam ${UfraEndOfSemester.countdown.days} dias e ${UfraEndOfSemester.countdown.hours} horas para o final do semestre! Continue estudando!`;
+}
 
 async function translate(text, target) {
    const response = await fetch(
@@ -36,10 +47,38 @@ export async function getDog() {
    return dog;
 }
 
-export async function getVerse(book, chapterVerse) {
-   const response = await fetch(
-      `https://bible-api.com/${book}+${chapterVerse}?translation=almeida`
-   );
+export async function clean(message) {
+   if (!message.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
+      message.reply("Você não tem permissão para deletar mensagens.");
+      return;
+   }
+
+   const args = message.content.split(" ");
+   const numMessages = Number(args[1]);
+
+   if (isNaN(numMessages) || numMessages <= 0) {
+      message.reply("Forneça um número válido de mensagens a serem deletadas.\nExemplo: `!clean 5`.");
+      return;
+   } else if (numMessages > 99) {
+      message.reply("Você só pode deletar no máximo 99 mensagens de uma vez.");
+      return;
+   }
+
+   await message.channel.bulkDelete(numMessages + 1, true);
+   await message.channel.send(`${numMessages} mensagens foram deletadas!`);
+}
+
+export async function getVerse(message) {
+   console.log(message.content);
+   const args = message.content.split(" ");
+   if (args.length < 3) {
+      message.reply("Defina o livro, capítulo e versículo.\nExemplo: `!bible genesis 1:1`.");
+      return;
+   }
+   const book = args[1];
+   const chapterVerse = args[2];
+
+   const response = await fetch(`https://bible-api.com/${book}+${chapterVerse}?translation=almeida`);
    const jsonData = await response.json();
 
    const reference = jsonData.reference;
@@ -48,19 +87,14 @@ export async function getVerse(book, chapterVerse) {
    return text + "\n\n" + reference;
 }
 
-export function ferias() {
-   const UfraEndOfSemester = new Countdown("19 May 2023 23:59:59 GMT-0300");
+export async function getWiki(message) {
+   const argArray = message.content.split(" ").slice(1);
+   const expression = argArray.join("_");
 
-   return `Faltam ${UfraEndOfSemester.countdown.days} dias e ${UfraEndOfSemester.countdown.hours} horas para o final do semestre! Continue estudando!`;
-}
-
-export async function getWiki(arg) {
-   const resolve = await fetch(
-      `https://pt.wikipedia.org/api/rest_v1/page/summary/${arg}`
-   );
+   const resolve = await fetch(`https://pt.wikipedia.org/api/rest_v1/page/summary/${expression}`);
    const jsonData = await resolve.json();
    const summary =
       (jsonData && jsonData.extract) ||
-      `Não consegui encontrar o termo ${arg}. Você digitou corretamente?`;
+      `Não consegui encontrar o termo ${message.content}. Você digitou corretamente?`;
    return summary;
 }
--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-const {
-   ActivityType,
-   Client,
-   GatewayIntentBits,
-   PermissionsBitField,
-} = require("discord.js");
-const {
-   getQuote,
-   getCat,
-   getDog,
-   getVerse,
-   ferias,
-   getWiki,
-} = require("./functions.js");
-require("dotenv/config");
+import { ActivityType, Client, GatewayIntentBits } from "discord.js";
+import { getQuote, getCat, getDog, getVerse, clean, ferias, getWiki, getHelp } from "./functions.js";
+import "dotenv/config";
 
 const client = new Client({
    intents: [
@@ -30,79 +18,21 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", async (message) => {
-   const responses = {
-      "!help": () =>
-         `Olá, ${message.author.username}!\nComandos legais:\n\n\`!cookie\`: frase inspiradora de autores famosos.\n\`!clean\`: limpa mensagens recentes do chat atual.\n\`!cat\`: imagens aleatórias de gatinhos.\n\`!dog\`: imagens e gifs aleatórios de doguinhos.\n\`!bible\`: versículos da bíblia.\n\nO malvadão ainda está trabalhando em mais comandos.`,
-      "!cookie": getQuote,
-      "!cat": getCat,
-      "!dog": getDog,
-      "!ferias": ferias,
-   };
-
-   if (message.content === "oi") {
+   if (message.content === "!help") message.reply(getHelp(message));
+   else if (message.content === "!ferias") message.reply(ferias());
+   else if (message.content === "!cookie") message.reply(await getQuote());
+   else if (message.content === "!cat") message.reply(await getCat());
+   else if (message.content === "!dog") message.reply(await getDog());
+   else if (message.content.startsWith("!clean")) await clean(message);
+   else if (message.content.startsWith("!bible")) message.reply(await getVerse(message));
+   else if (message.content.startsWith("!wiki")) message.reply(await getWiki(message));
+   // random
+   else if (message.content === "oi") {
       if (Math.random() <= 0.2) message.reply("oi BANDIDO");
    } else if (message.content === "perdi") {
       if (Math.random() <= 0.2) message.reply("posso te ajudar a encontrar?");
    } else if (message.content === "mlk") {
       if (Math.random() <= 0.2) message.reply("martin luther king?");
-   }
-
-   if (message.content.startsWith("!clean")) {
-      // Verifica se o usuário tem permissão para deletar mensagens
-      if (
-         !message.member.permissions.has(
-            PermissionsBitField.Flags.ManageMessages
-         )
-      ) {
-         message.reply("Você não tem permissão para deletar mensagens.");
-         return;
-      }
-
-      // Separa o comando da quantidade de mensagens a serem deletadas
-      const args = message.content.split(" ");
-      // Pega a quantidade de mensagens e transforma em número
-      const numMessages = Number(args[1]);
-
-      if (isNaN(numMessages) || numMessages <= 0) {
-         message.reply(
-            "Forneça um número válido de mensagens a serem deletadas.\nExemplo: `!clean 5`."
-         );
-         return;
-      } else if (numMessages > 99) {
-         message.reply(
-            "Você só pode deletar no máximo 99 mensagens de uma vez."
-         );
-         return;
-      }
-
-      await message.channel.bulkDelete(numMessages + 1, true);
-      await message.channel.send(`${numMessages} mensagens foram deletadas!`);
-   }
-
-   if (message.content.startsWith("!bible")) {
-      const args = message.content.split(" ");
-      if (args.length < 3) {
-         message.reply(
-            "Defina o livro, capítulo e versículo.\nExemplo: `!bible genesis 1:1`."
-         );
-         return;
-      }
-      const book = args[1];
-      const chapterVerse = args[2];
-
-      message.reply(await getVerse(book, chapterVerse));
-   }
-
-   if (message.content.startsWith("!wiki")) {
-      const argArray = message.content.split(" ").slice(1);
-      const expression = argArray.join("_");
-      return message.reply(await getWiki(expression));
-   }
-
-   const finalMessage = message.content.toLowerCase();
-
-   if (responses[finalMessage]) {
-      message.reply(await responses[finalMessage]());
    }
 });
 
