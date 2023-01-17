ba00806d648a925049f547eef85a71e812d8181c
Laion Gabriel <laiong.dev@gmail.com>
2023-01-17 17:26:55 -0300

    add AI

---
+++ ai.js
@@ -0,0 +1,26 @@
+import { Configuration, OpenAIApi } from "openai";
+import "dotenv/config";
+
+const configuration = new Configuration({
+   apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export async function ask(message) {
+   const prompt = message.content.slice(1);
+   try {
+      const response = await openai.createCompletion({
+         model: "text-davinci-003",
+         prompt,
+         temperature: 0.6,
+         max_tokens: 512,
+         top_p: 0.9,
+         frequency_penalty: 0,
+         presence_penalty: 0,
+      });
+      const answer = response.data.choices[0].text;
+      return answer;
+   } catch (error) {
+      console.log(error);
+   }
+}
