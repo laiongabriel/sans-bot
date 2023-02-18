07ead8d6c4235aa74c65d70185c1d93e5bd0b5f7
Laion Gabriel <laiong.dev@gmail.com>
2023-02-18 18:00:03 -0300

    add sendTyping function

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import { ActivityType, Client, GatewayIntentBits } from "discord.js";
-import { getQuote, getCat, getDog, getVerse, clean, ferias, getWiki, getHelp } from "./functions.js";
+import {
+   getQuote,
+   getCat,
+   getDog,
+   getVerse,
+   clean,
+   ferias,
+   getWiki,
+   getHelp,
+} from "./functions.js";
 import { ask, getAiImage } from "./ai.js";
 import "dotenv/config";
 
@@ -24,25 +33,30 @@ client.on("reconnecting", () => {
    });
 });
 
+async function sendTypingAndReply(message, reply) {
+   message.channel.sendTyping();
+   message.reply(await reply);
+}
+
 client.on("messageCreate", async (message) => {
-   if (message.content === "!help") message.reply(getHelp(message));
-   else if (message.content === "!ferias") message.reply(ferias());
-   else if (message.content === "!cookie") message.reply(await getQuote());
-   else if (message.content === "!cat") message.reply(await getCat());
-   else if (message.content === "!dog") message.reply(await getDog());
-   else if (message.content.startsWith("!clean")) await clean(message);
-   else if (message.content.startsWith("!bible")) message.reply(await getVerse(message));
-   else if (message.content.startsWith("!wiki")) message.reply(await getWiki(message));
-   else if (message.content.startsWith("!img")) message.reply(await getAiImage(message));
-   else if (message.content.startsWith("%")) message.reply(await ask(message));
-   // random
-   else if (message.content === "oi") {
-      if (Math.random() <= 0.2) message.reply("oi BANDIDO");
-   } else if (message.content === "perdi") {
-      if (Math.random() <= 0.2) message.reply("posso te ajudar a encontrar?");
-   } else if (message.content === "mlk") {
-      if (Math.random() <= 0.2) message.reply("martin luther king?");
-   }
+   if (message.content === "!help") sendTypingAndReply(message, getHelp(message));
+   else if (message.content === "!ferias") sendTypingAndReply(message, ferias());
+   else if (message.content === "!cookie") sendTypingAndReply(message, getQuote());
+   else if (message.content === "!cat") sendTypingAndReply(message, getCat());
+   else if (message.content === "!dog") sendTypingAndReply(message, getDog());
+   else if (message.content.startsWith("!clean")) {
+      message.channel.sendTyping();
+      await clean(message);
+   } else if (message.content.startsWith("!bible")) sendTypingAndReply(message, getVerse(message));
+   else if (message.content.startsWith("!wiki")) sendTypingAndReply(message, getWiki(message));
+   else if (message.content.startsWith("!img")) sendTypingAndReply(message, getAiImage(message));
+   else if (message.content.startsWith("%")) sendTypingAndReply(message, ask(message));
+   else if (message.content === "oi" && Math.random() <= 0.2)
+      sendTypingAndReply(message, "oi BANDIDO");
+   else if (message.content === "perdi" && Math.random() <= 0.2)
+      sendTypingAndReply(message, "posso te ajudar a encontrar?");
+   else if (message.content === "mlk" && Math.random() <= 0.2)
+      sendTypingAndReply(message, "martin luther king?");
 });
 
 client.login(process.env.TOKEN);
